869c586ceed6d79ad88a208f5952801ee54c36b7
Fredi <fredisoft@gmail.com>
2012-12-19 11:39:03 -0200

    Initial commit

---
+++ BowlingGameKata.Tests/GameTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BowlingGameKata;
+
+namespace BowlingGameKata.Tests
+{
+    public class GameTests
+    {
+        Game g;
+
+        public GameTests()
+        {
+            g = new Game();
+        }
+
+        private void rollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+
+        private void rollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+
+        private void rollStrike()
+        {
+            g.Roll(10);
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            rollMany(20, 0);
+
+            Assert.Equal(0, g.Score());
+        }
+
+        [Fact]
+        public void AllOnes()
+        {
+            rollMany(20, 1);
+
+            Assert.Equal(20, g.Score());
+        }
+
+        [Fact]
+        public void OneSpare()
+        {
+            rollSpare();
+            g.Roll(3);
+
+            rollMany(17, 0);
+
+            Assert.Equal(16, g.Score());
+        }
+
+        [Fact]
+        public void OneStrike()
+        {
+            rollStrike();
+            g.Roll(3);
+            g.Roll(4);
+
+            rollMany(16, 0);
+
+            Assert.Equal(24, g.Score());
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            rollMany(12, 10);
+
+            Assert.Equal(300, g.Score());
+        }
+
+        [Fact]
+        public void AllFives()
+        {
+            rollMany(21, 5);
+
+            Assert.Equal(150, g.Score());
+        }
+    }
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        private bool isSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+        }
+
+        private bool isStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame] == 10;
+        }
+
+        private int nextTwoBallsForStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+        }
+
+        private int nextBallForSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame + 2];
+        }
+
+        private int twoBallsInFrame(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1];
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int firstInFrame = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(firstInFrame))
+                {
+                    score += 10 + nextTwoBallsForStrike(firstInFrame);
+                    firstInFrame++;
+                }
+                else if (isSpare(firstInFrame))
+                {
+                    score += 10 + nextBallForSpare(firstInFrame);
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += twoBallsInFrame(firstInFrame);
+                    firstInFrame += 2;
+                }
+            }
+
+            return score;
+        }
+    }
+}
